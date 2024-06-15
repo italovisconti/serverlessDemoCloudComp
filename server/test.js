70e98569999fec032d51e1e615f731e882b2e3d0
italovisconti <hermanosvisconti@hotmail.com>
2024-06-15 16:12:43 -0400

    Update test.js

--- server/test.js
+++ server/test.js
@@ -14,7 +14,7 @@
 describe("basics", () => {
   it("test add", async () => {
     const axios = require("axios").default;
-    res = await axios.post("http://localhost:3000/dev/calcs/add", {
+    const res = await axios.post("http://localhost:3000/dev/calcs/add", {
       numbers: [1, 1, 2, 3, 5],
     });
     expect(res.data.result).toBe(12);
